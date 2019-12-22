95a8155f00fba1a4d7960a054ffe62679297e3d1
Adam Dobrawy <ad-m@users.noreply.github.com>
2019-12-22 02:38:48 +0100

    Add support for function as branch node in tree

--- src/omelette.js
+++ src/omelette.js
@@ -111,16 +111,18 @@
         depth = depthOf(objectTree);
         for (level = i = 1, ref = depth; (1 <= ref ? i <= ref : i >= ref); level = 1 <= ref ? ++i : --i) {
           this.on(`$${level}`, function({fragment, reply, line}) {
-            var accessor, lastIndex, replies;
+            var lastIndex, replies;
             if (!(/\s+/.test(line.slice(-1)))) {
               lastIndex = -1;
             }
-            accessor = t => line.split(/\s+/).slice(1, lastIndex).filter(Boolean).reduce((a, v) => a[v], t);
-            replies = fragment === 1 ? Object.keys(objectTree) : accessor(objectTree);
+            if (fragment === 1) {
+              replies = Object.keys(objectTree);
+            } else {
+              replies = objectTree;
+              accessor = t => line.split(/\s+/).slice(1, lastIndex).filter(Boolean).reduce((a, v) => a[v] instanceof Function ? a[v]() : a[v], t);
+              replies = fragment === 1 ? Object.keys(objectTree) : accessor(objectTree);
+            };
             return reply((function(replies) {
-              if (replies instanceof Function) {
-                return replies();
-              }
               if (replies instanceof Array) {
                 return replies;
               }
